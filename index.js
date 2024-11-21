3169c9eaba3f2195893da21252ebb7d72be22315
salehiin <salehin.ay@gmail.com>
2024-11-21 23:33:13 +0600

    delete api ready

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrbkxoj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,25 +32,35 @@ async function run() {
     const guidesCollection = client.db("pathfindrDB").collection("guides");
     const cartCollection = client.db("pathfindrDB").collection("carts");
 
-    app.get('/packages', async(req, res) =>{
-        const result = await packagesCollection.find().toArray();
-        res.send(result);
+    app.get('/packages', async (req, res) => {
+      const result = await packagesCollection.find().toArray();
+      res.send(result);
     })
 
-    app.get('/guides', async(req, res) =>{
-        const result = await guidesCollection.find().toArray();
-        res.send(result);
+    app.get('/guides', async (req, res) => {
+      const result = await guidesCollection.find().toArray();
+      res.send(result);
     })
 
     // carts collection
-    app.get('/carts', async(req, res) =>{
-      const result = await cartCollection.find().toArray();
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.post('/carts', async(req, res) =>{
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    // data delete from cart
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -66,9 +76,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('pathfindr is exploring')
+  res.send('pathfindr is exploring')
 })
 
-app.listen(port, () =>{
-    console.log(`Pathfindr is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Pathfindr is running on port ${port}`);
 })
